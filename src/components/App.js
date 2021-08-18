d42fa065d6f93e92a3d1875300bddfa6cffeb02a
vabs10 <vyagneshwar98@gmail.com>
2021-08-18 19:06:29 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  let name = "Vaibhav";
   return (
-    <Welcome name="" />
+    <Welcome name={name} />
   )
 }
 
